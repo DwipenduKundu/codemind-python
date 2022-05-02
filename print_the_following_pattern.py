1d4fd1474482dfc6b6afcb971be88bfa588e9a20
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-05-03 00:44:40 +0530

    a=int(input("")) for i in range(a):     for j in range(a):         if(i==j):             print("0",end="")         else:             print("x",end="")     print("")

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 a=int(input(""))
-for i in range(1,a+1):
-    for j in range(1,i+1):
-        print(j,end="")
+for i in range(a):
+    for j in range(a):
+        if(i==j):
+            print("0",end="")
+        else:
+            print("x",end="")
     print("")
