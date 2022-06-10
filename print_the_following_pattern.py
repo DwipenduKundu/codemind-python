84f8b2dd38e13442b9881772ee5a4d60da361053
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-06-11 00:36:30 +0530

    n=int(input()) c=chr(64+n) for i in range(n,0,-1):     for j in range(1,i+1):         print(c,end=" ")     c=chr(ord(c) - 1)     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,7 @@
 n=int(input())
-c="A"
-for i in range(1,n+1):
-    for j in range(1,n+1):
+c=chr(64+n)
+for i in range(n,0,-1):
+    for j in range(1,i+1):
         print(c,end=" ")
-    c=chr(ord(c) + 1)
+    c=chr(ord(c) - 1)
     print()
