1a6be8eb70a9ce27bd1e5d3e11059887d954e6c5
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-12-22 15:11:10 +0530

    n=int(input()) arr=list(map(int,input().split())) o=[] e=[] f=[] for i in arr:     if i%2==0:         e.append(i)     else:         o.append(i) if len(e)>len(o):     l=len(o) else:     l=len(e) for i in range(l):     f.append(e[i])     f.append(o[i]) if len(e)>len(o):     for i in range(len(o),len(e)):         f.append(e[i]) elif len(e)<len(o):     for i in range(len(e),len(o)):         f.append(o[i]) if n%2!=0:     f.append(0) print(*f)

---
+++ even_odd_pairs.py
@@ -0,0 +1,26 @@
+n=int(input())
+arr=list(map(int,input().split()))
+o=[]
+e=[]
+f=[]
+for i in arr:
+    if i%2==0:
+        e.append(i)
+    else:
+        o.append(i)
+if len(e)>len(o):
+    l=len(o)
+else:
+    l=len(e)
+for i in range(l):
+    f.append(e[i])
+    f.append(o[i])
+if len(e)>len(o):
+    for i in range(len(o),len(e)):
+        f.append(e[i])
+elif len(e)<len(o):
+    for i in range(len(e),len(o)):
+        f.append(o[i])
+if n%2!=0:
+    f.append(0)
+print(*f)
