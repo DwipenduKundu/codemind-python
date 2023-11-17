0834d306dec07e716b4b97be13d00e3ecf3cac43
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2023-11-17 09:02:59 +0530

    n=int(input()) np2=n+1 while str(np2)!=str(np2)[::-1]:     np2+=1 np1=n-1 while str(np1)!=str(np1)[::-1]:     np1-=1 if abs(n-np1)==abs(n-np2):     print(np1,np2) elif abs(n-np1)>abs(n-np2):     print(np2) else:     print(np1)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,19 +1,13 @@
-a=int(input())
-np=a+1
-while 1:
-    if np==int(str(np)[::-1]):
-        f1=np
-        break
-    np+=1
-np=a-1
-while 1:
-    if np==int(str(np)[::-1]):
-        f2=np
-        break
-    np-=1
-if abs(a-f2)>abs(a-f1):
-    print(f1)
-elif abs(a-f2)<abs(a-f1):
-    print(f2)
-elif abs(a-f2)==abs(a-f1):
-    print(f2,f1)
+n=int(input())
+np2=n+1
+while str(np2)!=str(np2)[::-1]:
+    np2+=1
+np1=n-1
+while str(np1)!=str(np1)[::-1]:
+    np1-=1
+if abs(n-np1)==abs(n-np2):
+    print(np1,np2)
+elif abs(n-np1)>abs(n-np2):
+    print(np2)
+else:
+    print(np1)
