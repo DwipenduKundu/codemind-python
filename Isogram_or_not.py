8e29942a0865aee34e971504684a6cb45c6e9063
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2023-11-17 09:25:59 +0530

    s=input() f=0 for i in s:     if s.count(i)!=1:         f=1         break if f==0:     print(True) else:     print(False)

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,9 +1,10 @@
-str=input()
-sl=list(str)
-sl.sort()
-ss=list(set(str))
-ss.sort()
-if ss==sl:
+s=input()
+f=0
+for i in s:
+    if s.count(i)!=1:
+        f=1
+        break
+if f==0:
     print(True)
 else:
     print(False)
