e523b1de626c092ea9004321796f8a34a45b12a2
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2023-11-17 08:46:58 +0530

    def convert(octal):     i = 0     decimal = 0     while octal != 0:         digit = octal % 10         decimal += digit * pow(8, i)         octal //= 10         i += 1     binary = 0     rem = 0     i = 1
        while decimal != 0:
            rem = decimal % 2
            decimal //= 2
            binary += rem * i
            i *= 10
        print(binary)
    
    
    octal = int(input())
    convert(octal)

---
+++ octal_to_binary.py
@@ -0,0 +1,22 @@
+def convert(octal):
+    i = 0
+    decimal = 0
+    while octal != 0:
+        digit = octal % 10
+        decimal += digit * pow(8, i)
+        octal //= 10
+        i += 1
+    binary = 0
+    rem = 0
+    i = 1
+
+    while decimal != 0:
+        rem = decimal % 2
+        decimal //= 2
+        binary += rem * i
+        i *= 10
+    print(binary)
+
+
+octal = int(input())
+convert(octal)
